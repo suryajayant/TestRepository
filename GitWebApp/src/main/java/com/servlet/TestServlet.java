80e0a0caa36bfd4c3e80fad0b5e8d4385db787ad
Jayant <suryajayant@gmail.com>
2017-11-21 17:03:40 +0530

    V1.1.1

--- GitWebApp/src/main/java/com/servlet/TestServlet.java
+++ GitWebApp/src/main/java/com/servlet/TestServlet.java
@@ -13,6 +13,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doPost(request, response);
+		//
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
