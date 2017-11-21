00f089f0ad584defa948971195031ff7ec0f045b
Jayant <suryajayant@gmail.com>
2017-11-21 15:57:07 +0530

    v1.0

---
+++ GitWebApp/src/main/java/com/servlet/TestServlet.java
@@ -0,0 +1,25 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String param1= request.getParameter("param1");
+		PrintWriter out= response.getWriter();
+		out.print("You Entered: "+ param1);
+	}
+
+}
---
+++ GitWebApp/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="TestServlet">
+	<input type='text' name="param1"/>
+	<input type="submit" value="Submit"/>
+</form>
+</body>
+</html>
